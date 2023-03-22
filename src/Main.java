a655dd92dd5144bea198b4af629219ba0901f8a4
AndyDig <126315776+AndyDig@users.noreply.github.com>
2023-03-23 01:00:31 +0300

    DZ3.8

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        var a = "4";
+        if (a == "test") {
+            System.out.println("Верно");
+        } else {
+            System.out.println("Неверно");
+        }
+    }
+}
